ceba1b2c2f62b944620ff1b7b5a2f676e17293f5
Luiz Guilherme Guizi <guilhermeluizguizi@outlook.com>
2024-11-20 17:07:54 -0300

    first deploy

--- App.js
+++ App.js
@@ -1,20 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View, Pressable, Keyboard } from 'react-native';
+
+//Components
+import Titulo from './src/components/title/Title';
+import From from './src/components/form/resultimc/From';
+import Historic from './src/components/historic/Historic';
+
+//navigation
+import {NavigationContainer}  from '@react-navigation/native'
+import {createStackNavigator} from '@react-navigation/stack'
 
 export default function App() {
+  const Stack = createStackNavigator();
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Pressable onPress={Keyboard.dismiss} style={styles.container}>
+      <NavigationContainer> 
+        <Stack.Navigator initialRouteName="From">
+          <Stack.Screen name="From" component={From} />
+          <Stack.Screen name="Historic" component={Historic} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </Pressable>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#f47709',
+    paddingTop: 50,
+    borderRadius: 30,
   },
 });
